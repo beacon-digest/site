4e52a06a2ef246ab33ff12c08901559e9cc57545
Willman Duffy <willmanduffy@gmail.com>
2025-12-23 12:44:09 -0500

    Another timezone change

--- app/utils/events.ts
+++ app/utils/events.ts
@@ -1,17 +1,16 @@
+import { formatInTimeZone } from "date-fns-tz";
+
 export const formatEventTime = (
   startAt: Date | null,
   endAt: Date | null,
 ): string => {
   if (!startAt) return "Time TBD";
 
-  const formatOptions: Intl.DateTimeFormatOptions = {
-    hour: "numeric",
-    minute: "2-digit",
-    hour12: true,
-  };
+  const TIME_ZONE = "America/New_York";
+  const timeFormat = "h:mm a"; // e.g., "5:30 PM"
 
-  const startTime = startAt.toLocaleTimeString("en-US", formatOptions);
-  const endTime = endAt?.toLocaleTimeString("en-US", formatOptions);
+  const startTime = formatInTimeZone(startAt, TIME_ZONE, timeFormat);
+  const endTime = endAt ? formatInTimeZone(endAt, TIME_ZONE, timeFormat) : null;
 
   return endTime ? `${startTime} - ${endTime}` : startTime;
 };
